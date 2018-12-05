eb29f7771b90ee4b2e8bcb0330bf65a1c58b91b1
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-12-05 16:23:34 +0700

    Commit project

---
+++ QLCafe/QLCafe/DAO/TableDAO.cs
@@ -0,0 +1,49 @@
+﻿using QLCafe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCafe.DAO
+{
+    class TableDAO
+    {
+        private static TableDAO instance;
+        public static TableDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new TableDAO();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+
+
+        private TableDAO() { }
+
+        public static int TableWidth = 90;
+        public static int TableHeight = 90;
+
+
+        public List<Table> LoadTableList()
+        {
+            List<Table> tableList = new List<Table>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery("USP_GetTableList");
+
+            foreach (DataRow item in data.Rows)
+            {
+                Table table = new Table(item);
+                tableList.Add(table);
+            }
+
+            return tableList;
+        }
+    }
+}
